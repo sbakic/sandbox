4424e1e33dcab822d6889a1df4ffb7e56925263d
Stefan Bakic <sbakic93@gmail.com>
2024-11-09 13:46:30 +0100

    Fix import

--- opentelemetry-nestjs/src/app.module.ts
+++ opentelemetry-nestjs/src/app.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { DiceController } from 'src/dice/dice.controller';
+import {Module} from '@nestjs/common';
+import {DiceController} from "./dice/dice.controller";
 
 @Module({
     imports: [],
