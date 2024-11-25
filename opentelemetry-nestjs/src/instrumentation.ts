ee453d0208604665303d70156ba5fb420e0bfbe4
Stefan Bakic <sbakic93@gmail.com>
2024-11-25 17:59:02 +0100

    Add opentelemetry-nestjs resource

--- opentelemetry-nestjs/src/instrumentation.ts
+++ opentelemetry-nestjs/src/instrumentation.ts
@@ -1,12 +1,18 @@
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { Resource } from '@opentelemetry/resources';
 import { ConsoleMetricExporter, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const opentelemetry: NodeSDK = new NodeSDK({
+    resource: new Resource({
+        [ATTR_SERVICE_NAME]: 'sandbox-nestjs',
+        [ATTR_SERVICE_VERSION]: '1.0.0',
+    }),
     traceExporter: new ConsoleSpanExporter(),
     metricReader: new PeriodicExportingMetricReader({
         exporter: new ConsoleMetricExporter(),
